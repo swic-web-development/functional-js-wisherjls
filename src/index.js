7592b0008d569ff3ff71a1b863764b0a3dbd2308
wisher <wisherjls@gmail.com>
2025-03-05 21:33:07 -0600

    callback functions

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@
 // }
 
 //const calcOrderCost = (order) => order.quantity * order.priceEach
+
+
+function doSomethingWithNumber(number, callbackFunction) {
+  return callbackFunction(number)
+}
+
+function divideByTwo(num) {
+  return num / 2
+}
+
+function double(num) {
+  return num * 2
+}
+
+function triple(num) {
+  return num * 3
+}
+//369 is the number that we want to do something with.
+//double is the callback function that we want to do with the number.
+console.log(doSomethingWithNumber(369, double))  
+console.log(doSomethingWithNumber(369, triple))
+console.log(doSomethingWithNumber(369, divideByTwo))

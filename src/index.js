fe2b206bbbb798513b1cee5c17610642217c476f
wisher <wisherjls@gmail.com>
2025-03-05 21:59:05 -0600

    the map method in action backpack Full of ammo

--- src/index.js
+++ src/index.js
@@ -12,26 +12,26 @@
 //const calcOrderCost = (order) => order.quantity * order.priceEach
 
 
-function doSomethingWithNumber(number, callbackFunction) {
-  return callbackFunction(number)
-}
+//function doSomethingWithNumber(number, callbackFunction) {
+  //return callbackFunction(number)
+//}
 
-function divideByTwo(num) {
-  return num / 2
-}
+//function divideByTwo(num) {
+  //return num / 2
+//}
 
-function double(num) {
-  return num * 2
-}
+//function double(num) {
+  //return num * 2
+//}
 
-function triple(num) {
-  return num * 3
-}
+//function triple(num) {
+  /return num * 3
+//}
 //369 is the number that we want to do something with.
 //double is the callback function that we want to do with the number.
-console.log(doSomethingWithNumber(369, double))  
-console.log(doSomethingWithNumber(369, triple))
-console.log(doSomethingWithNumber(369, divideByTwo))
+//console.log(doSomethingWithNumber(369, double))  
+//console.log(doSomethingWithNumber(369, triple))
+//console.log(doSomethingWithNumber(369, divideByTwo))
 
 
 
@@ -55,3 +55,14 @@ const doubledNumbers = numbers.map(function (number) {
 })
 
 console.log(doubledNumbers)
+
+const backpackFullOfAmmo = [
+{item: 'bullets', quantity: 10, priceEach: 0.5},
+{item: 'shoutgun shells', quantity: 4, priceEach: 0.5},
+{item: 'rockets', quantity: 1, priceEach: 5.0},
+{item: 'energy cell units', quantity: 20, priceEach: 0.2}
+]
+
+const calculateOrderCost = (order) => order.quantity * order.priceEach
+
+backpackFullOfAmmo.map((ammo) => {console.log(ammo)})
